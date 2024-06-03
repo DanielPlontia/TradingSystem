418d00593644d610095afce12c63b002d2af34ae
DanielPlontia <plontia@gmail.com>
2024-06-03 14:58:34 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Trading System
+---
+Trading System KATA
---
+++ Sample-Test1/tradingSystem_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../TradingSystem/StockBrockerDriver.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
