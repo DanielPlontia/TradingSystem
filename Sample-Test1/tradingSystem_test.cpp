b9c8f54f940dfbe3805e844ed76f7e4cd47f22d6
DanielPlontia <plontia@gmail.com>
2024-06-03 15:11:27 +0900

    Add login TestCase

--- Sample-Test1/tradingSystem_test.cpp
+++ Sample-Test1/tradingSystem_test.cpp
@@ -2,7 +2,30 @@
 #include "gmock/gmock.h"
 #include "../TradingSystem/StockBrockerDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace testing;
+
+class StockMock : public StockDriver {
+public:
+	MOCK_METHOD(bool, login, (int id, int pass), (override));
+	MOCK_METHOD(void, buy, (int code, int price, int count), (override));
+	MOCK_METHOD(void, sell, (int code, int price, int count), (override));
+	MOCK_METHOD(int, getPrice, (int code, time_t time), (override));
+};
+
+class StockFixture : public Test {
+	StockMock mock;
+	StockBrockerDriver sbd{ &mock };
+};
+
+TEST_F(StockBrockerTest, LoginSucess) {
+	int id = 1234;
+	int pass = 0000;
+
+	EXPECT_CALL(mock, login)
+		.WillRepeatedly(Return(true));
+
+	bool bSuccess = sbd.login(id, pass);
+
+	EXPECT_THAT(bSuccess, Eq(true));
+
 }
