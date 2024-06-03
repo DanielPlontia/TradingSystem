4e42e912172929f85712216966092721f2f03e3e
DESKTOP-5FBE16T\User <kimki1117@gmail.com>
2024-06-03 16:15:03 +0900

    Add sell function

--- TradingSystem/StockBrockerDriver.cpp
+++ TradingSystem/StockBrockerDriver.cpp
@@ -1,2 +1,25 @@
+#include <stdexcept>
 #include "StockDriver.h"
 
+
+class StockBrockerDriver {
+private:
+	StockDriver* userStock;
+
+public:
+	StockBrockerDriver(StockDriver *stockDriver):
+	userStock(stockDriver){
+
+	}
+
+	void sell(int code, int price, int count) {
+		time_t now = std::time(nullptr);
+		userStock->sell(code, price, count);
+		int selledprice = userStock->getPrice(code, now);
+		if (price != selledprice)
+			throw std::logic_error("주식이 정상적으로 팔리지 않았습니다.");
+	}
+
+
+
+};
