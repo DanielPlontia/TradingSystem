685d680ce3b5d9261e9abd65a5b69617c5c13180
DESKTOP-5FBE16T\User <intelhs@naver.com>
2024-06-03 16:16:45 +0900

    factory & kiwi, nemo driver

--- TradingSystem/StockBrockerDriver.cpp
+++ TradingSystem/StockBrockerDriver.cpp
@@ -1,2 +1,57 @@
 #include "StockDriver.h"
+#include <string>
 
+using std::string;
+
+class KiwerStockDriver : public StockDriver {
+	// StockDriver을(를) 통해 상속됨
+	bool login(int id, int pass) override
+	{
+
+
+		return false;
+	}
+	void buy(int code, int price, int count) override
+	{
+	}
+	void sell(int code, int price, int count) override
+	{
+	}
+	int getPrice(int code, time_t time) override
+	{
+		return 0;
+	}
+};
+
+class NemoStockDriver : public  StockDriver{
+	// StockDriver을(를) 통해 상속됨
+	bool login(int id, int pass) override
+	{
+		return false;
+	}
+	void buy(int code, int price, int count) override
+	{
+	}
+	void sell(int code, int price, int count) override
+	{
+	}
+	int getPrice(int code, time_t time) override
+	{
+		return 0;
+	}
+};
+
+class StockBrockerDriver {
+private:
+	StockDriver* userStock;
+
+public:
+
+	StockDriver* selectStockBrocker(string stock)
+	{
+		if (stock == "Kiwer") return new KiwerStockDriver();
+		if (stock == "Nemo") return new NemoStockDriver();
+		else return nullptr;
+	}
+
+};
